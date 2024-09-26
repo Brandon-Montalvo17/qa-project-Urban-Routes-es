5c0753fbbfcdbf8a0b6942f321ad8818863f2d56
Brandon Alejandro Montalvo <bmontalvo244@gmail.com>
2024-09-26 12:22:20 -0600

    Review before send

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 import data
 from selenium import webdriver
 from selenium.webdriver import Keys
@@ -38,6 +40,18 @@ class UrbanRoutesPage:
     from_field = (By.ID, 'from')
     to_field = (By.ID, 'to')
 
+    call_taxi_button = (By.CLASS_NAME, "button round")
+    Comfort_button = (By.XPATH, "//*[@id='root'] /div /div[3] / div[3] / div[2] / div[1] / div[5] / div[2]")
+    phone_number_button = (By.CLASS_NAME, "np-text")
+    phone_input = (By.ID, "phone")
+    phone_send = (By.XPATH, "//*[@id='rest'] / div / [div1] / [div2] / [div1] / form / div[2] / button")
+    pay_method = (By.CLASS_NAME, "pp-text")
+    add_card_button = (By.CLASS_NAME, "pp-title")
+    add_card = (By.XPATH, "//*[text() = 'Agregar Tarjeta']")
+    add_card_data = (By.ID, "number")
+    cvv = (By.XPATH, "//*[@id='code']")
+
+
     def __init__(self, driver):
         self.driver = driver
 
@@ -53,11 +67,53 @@ class UrbanRoutesPage:
     def get_to(self):
         return self.driver.find_element(*self.to_field).get_property('value')
 
+    def comfort_button_req(self):
+        time.sleep(2)
+        self.driver.find_element(*self.call_taxi_button).click()
+        time.sleep(2)
+        self.driver.find_element(*self.Comfort_button).click()
+
+    def phone_number_req(self):
+        self.driver.find_element(*self.phone_number_button).click()
+
+    def phone_number_fill(self):
+        self.driver.find_element(*self.phone_input).click()
+
+    def phone_number_send(self):
+        self.driver.find_element(*self.phone_send).click()
+
+
+    def pay_method_req(self):
+        self.driver.find_element(*self.pay_method).click()
+
+    def add_card_req(self):
+        self.driver.find_element(*self.add_card_button).click()
+
+    def add_card_fill(self):
+        self.driver.find_element(*self.add_card).click()
+
+    def add_card_numbers(self):
+        self.driver.find_element(*self.add_card_data).click()
+
+    def cvv_add(self):
+        self.driver.find_element(*self.add_card_data, *self.cvv).send_keys(data.card_number, data.card_code)
+
+
 
 
 class TestUrbanRoutes:
 
     driver = None
+
+    message_driver = (By.XPATH, "//*[@id='comment']")
+    message_send = (By.CSS_SELECTOR, "Comment")
+    request_button = (By.CLASS_NAME, "reqs-head")
+    manta_panuelos = (By.CLASS_NAME, "r-sw-label")
+    helados = (By.CLASS_NAME, "r-counter-label")
+    taxi_modal = (By.CLASS_NAME, "smart-button-main")
+
+
+
 
     @classmethod
     def setup_class(cls):
@@ -65,7 +121,7 @@ class TestUrbanRoutes:
         from selenium.webdriver import DesiredCapabilities
         capabilities = DesiredCapabilities.CHROME
         capabilities["goog:loggingPrefs"] = {'performance': 'ALL'}
-        cls.driver = webdriver.Chrome(desired_capabilities=capabilities)
+        cls.driver = webdriver.Chrome()
 
     def test_set_route(self):
         self.driver.get(data.urban_routes_url)
@@ -77,6 +133,10 @@ class TestUrbanRoutes:
         assert routes_page.get_to() == address_to
 
 
+
     @classmethod
     def teardown_class(cls):
         cls.driver.quit()
+
+
+
